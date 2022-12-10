f5743eb56b5f3ccef8707267f1c907e97b9fce0a
Parth Gajjar <parth@selcom.net>
2022-12-10 15:23:41 +0530

    [wip]: Finally started working

---
+++ resources/views/livewire/admin/project/create.blade.php
@@ -0,0 +1,11 @@
+<div>
+    <div class="container p-4 mx-auto">
+        <div class="p-8 bg-white rounded-md card">
+            <div class="flex justify-between mb-4">
+                <h2>
+                    <span class="border-b-2 border-indigo-400">Project</span>
+                </h2>
+            </div>
+        </div>
+    </div>
+</div>
